a4ae4b7a9746ecb5f2003fe90e55164cb408e123
tungt <tungt@uniquify.com>
2024-10-25 13:34:34 +0700

    first commit

---
+++ preprocess.py
@@ -0,0 +1,40 @@
+import fitz
+from tqdm.auto import tqdm # for progress bars, requires !pip install tqdm 
+
+# Get PDF document
+pdf_path = "rag.pdf"
+
+def text_formatter(text: str) -> str:
+    """Performs minor formatting on text."""
+    cleaned_text = text.replace("\n", " ").strip() # note: this might be different for each doc (best to experiment)
+
+    # Other potential text formatting functions can go here
+    return cleaned_text
+
+def open_and_read_pdf(pdf_path: str) -> list[dict]:
+    """
+    Opens a PDF file, reads its text content page by page, and collects statistics.
+
+    Parameters:
+        pdf_path (str): The file path to the PDF document to be opened and read.
+
+    Returns:
+        list[dict]: A list of dictionaries, each containing the page number
+        (adjusted), character count, word count, sentence count, token count, and the extracted text
+        for each page.
+    """
+    doc = fitz.open(pdf_path)  # open a document
+    pages_and_texts = []
+    for page_number, page in tqdm(enumerate(doc)):  # iterate the document pages
+        text = page.get_text()  # get plain text encoded as UTF-8
+        text = text_formatter(text)
+        pages_and_texts.append({"page_number": page_number,  
+                                "page_char_count": len(text),
+                                "page_word_count": len(text.split(" ")),
+                                "page_sentence_count_raw": len(text.split(". ")),
+                                "page_token_count": len(text) / 4,  # 1 token = ~4 chars, see: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+                                "text": text})
+    return pages_and_texts
+
+pages_and_texts = open_and_read_pdf(pdf_path=pdf_path)
+print(pages_and_texts[:2])
